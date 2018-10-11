21e30f4f3fb06ec462771f60c996fae1e25512d4
Iskander Sharipov <quasilyte@gmail.com>
2018-10-11 23:04:47 +0300

    re-write for efficiency and quality of error messages
    Fixes #6
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ checkers.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+type fileChecker interface {
+	Reset()
+	PushFile(*repoFile)
+	CheckFiles() []string
+}
+
+type checkerBase struct {
+	files []*repoFile
+}
+
+func (c *checkerBase) Reset() {
+	c.files = c.files[:0]
+}
+
+func (c *checkerBase) PushFile(f *repoFile) {
+	c.acceptFile(f)
+}
+
+func (c *checkerBase) acceptFile(f *repoFile) {
+	c.files = append(c.files, f)
+}
+
+func (c *checkerBase) tempFilenames() []string {
+	names := make([]string, len(c.files))
+	for i, f := range c.files {
+		names[i] = f.tempName
+	}
+	return names
+}
+
+func (c *checkerBase) filenameReplacer() *strings.Replacer {
+	oldnew := make([]string, 0, len(c.files)*2)
+	for _, f := range c.files {
+		oldnew = append(oldnew, f.tempName, f.origName)
+	}
+	return strings.NewReplacer(oldnew...)
+}
+
+func isDocumentationFile(filename string) bool {
+	switch filename {
+	case "README", "README.md", "README.txt",
+		"CONTRIBUTING", "CONTRIBUTING.md", "CONTRIBUTING.txt":
+		return true
+	}
+	return false
+}
+
+type misspellChecker struct{ checkerBase }
+
+func (c *misspellChecker) PushFile(f *repoFile) {
+	if isDocumentationFile(f.baseName) {
+		f.require.localCopy = true
+		c.acceptFile(f)
+	}
+}
+
+func (c *misspellChecker) CheckFiles() (warnings []string) {
+	args := []string{"-error", "true"}
+	args = append(args, c.tempFilenames()...)
+	out, err := exec.Command("misspell", args...).CombinedOutput()
+	if err != nil {
+		replacer := c.filenameReplacer()
+		lines := strings.Split(string(out), "\n")
+		for _, l := range lines {
+			if l == "" {
+				continue
+			}
+			warnings = append(warnings, replacer.Replace(l))
+		}
+	}
+	return warnings
+}
+
+type brokenLinkChecker struct{ checkerBase }
+
+func (c *brokenLinkChecker) PushFile(f *repoFile) {
+	if isDocumentationFile(f.baseName) {
+		f.require.localCopy = true
+		c.acceptFile(f)
+	}
+}
+
+func (c *brokenLinkChecker) CheckFiles() (warnings []string) {
+	out, err := exec.Command("liche", c.tempFilenames()...).CombinedOutput()
+	if err != nil {
+		replacer := c.filenameReplacer()
+		lines := strings.Split(string(out), "\n")
+		var filename string
+		for i := 0; i < len(lines); i++ {
+			l := lines[i]
+			if l == "" {
+				continue
+			}
+			if l[0] != '\t' {
+				filename = replacer.Replace(l)
+				continue
+			}
+			if !strings.Contains(l, "ERROR") {
+				continue
+			}
+			// Next line contains error info.
+			url := strings.TrimLeft(l, "\t ERROR")
+			i++
+			l = lines[i]
+			if strings.Contains(l, "no such file") || strings.Contains(l, "root directory is not specified") {
+				// Not interested in file lookups, since we're
+				// not doing real git cloning.
+				continue
+			}
+			w := fmt.Sprintf("%s: %s: %s", filename, url, strings.TrimSpace(l))
+			warnings = append(warnings, w)
+		}
+	}
+	return warnings
+}
+
+type unwantedFileChecker struct {
+	checkerBase
+	patterns map[string]*regexp.Regexp
+}
+
+func newUnwantedFileChecker() *unwantedFileChecker {
+	return &unwantedFileChecker{
+		patterns: map[string]*regexp.Regexp{
+			"Vim swap":         regexp.MustCompile(`^.*\.swp$`),
+			"Emacs autosave":   regexp.MustCompile(`^#.*#$`),
+			"Emacs backup":     regexp.MustCompile(`^.*~$`),
+			"Mac OS sys file":  regexp.MustCompile(`^\.DS_STORE$`),
+			"Windows sys file": regexp.MustCompile(`^Thumbs\.db$`),
+		},
+	}
+}
+
+func (c *unwantedFileChecker) CheckFiles() (warnings []string) {
+	for _, f := range c.files {
+		for kind, pat := range c.patterns {
+			if !pat.MatchString(f.baseName) {
+				continue
+			}
+			w := fmt.Sprintf("remove %s file: %s", kind, f.origName)
+			warnings = append(warnings, w)
+		}
+	}
+	return warnings
+}
--- main.go
+++ main.go
@@ -9,99 +9,27 @@ import (
 	"log"
 	"math"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"regexp"
 	"strings"
+	"time"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
-type linter struct {
-	token  string
-	user   string
-	repos  []string
-	ctx    context.Context
-	client *github.Client
-
-	verbose   bool
-	skipForks bool
-
-	tempDir string
-}
-
-type fileChecker interface {
-	CheckFile(filename string) []error
-}
-
-type documentationChecker struct{}
-
-type brokenLinksChecker struct{}
-
-type misspellChecker struct{}
-
-func (c *documentationChecker) CheckFile(filename string) []error {
-	var errs []error
-	errs = append(errs, (&brokenLinksChecker{}).CheckFile(filename)...)
-	errs = append(errs, (&misspellChecker{}).CheckFile(filename)...)
-	return errs
-}
-
-func (c *misspellChecker) CheckFile(filename string) []error {
-	out, err := exec.Command("misspell", "-error", "true", filename).CombinedOutput()
-	if err != nil {
-		lines := strings.Split(string(out), "\n")
-		var errs []error
-		for _, l := range lines {
-			if l == "" {
-				continue
-			}
-			errs = append(errs, errors.New(l))
-		}
-		return errs
-	}
-	return nil
-}
-
-func (c *brokenLinksChecker) CheckFile(filename string) []error {
-	out, err := exec.Command("liche", filename).CombinedOutput()
-	if err != nil {
-		lines := strings.Split(string(out), "\n")
-		var errs []error
-		for i := 0; i < len(lines); i++ {
-			l := lines[i]
-			if !strings.Contains(l, "ERROR") {
-				continue
-			}
-			// Next line contains error info.
-			url := strings.TrimLeft(l, "\t ERROR")
-			i++
-			l = lines[i]
-			if strings.Contains(l, "no such file") || strings.Contains(l, "root directory is not specified") {
-				// Not interested in file lookups, since we're
-				// not doing real git cloning.
-				continue
-			}
-			errs = append(errs, errors.New(url+": "+strings.TrimSpace(l)))
-		}
-		return errs
-	}
-	return nil
-}
-
 func main() {
 	log.SetFlags(0)
-	var l linter
 
-	defer func() {
-		err := os.RemoveAll(l.tempDir)
-		if err != nil {
-			log.Printf("cleanup before exit: %v", err)
-		}
-	}()
+	l := linter{
+		checkers: map[string]fileChecker{
+			"broken link":   &brokenLinkChecker{},
+			"misspell":      &misspellChecker{},
+			"unwanted file": newUnwantedFileChecker(),
+		},
+	}
 
-	var steps = []struct {
+	defer l.cleanup()
+	steps := []struct {
 		name string
 		fn   func() error
 	}{
@@ -112,7 +40,6 @@ func main() {
 		{"get repos list", l.getReposList},
 		{"lint repos", l.lintRepos},
 	}
-
 	for _, step := range steps {
 		if err := step.fn(); err != nil {
 			log.Fatalf("%s: %v", step.name, err)
@@ -120,10 +47,53 @@ func main() {
 	}
 }
 
+type linter struct {
+	user  string
+	token string
+	repos []string
+
+	ctx    context.Context
+	client *github.Client
+
+	verbose      bool
+	skipForks    bool
+	skipInactive bool
+
+	checkers map[string]fileChecker
+
+	tempDir string
+}
+
+func (l *linter) cleanup() {
+	err := os.RemoveAll(l.tempDir)
+	if err != nil {
+		log.Printf("cleanup before exit: %v", err)
+	}
+}
+
 func (l *linter) initTempDir() error {
 	tempDir, err := ioutil.TempDir("", "repolint")
 	l.tempDir = tempDir
 	return err
+}
+
+func (l *linter) parseFlags() error {
+	flag.StringVar(&l.user, "user", "",
+		`github user/organization name`)
+	flag.BoolVar(&l.verbose, "v", false,
+		`verbose mode that turns on additional debug output`)
+	flag.BoolVar(&l.skipForks, `skipForks`, true,
+		`whether to skip repositories that are forks`)
+	flag.BoolVar(&l.skipInactive, `skipInactive`, true,
+		`whether to skip repositories with latest push dated more than 1 year ago`)
+
+	flag.Parse()
+
+	if l.user == "" {
+		return errors.New("-user argument can't be empty")
+	}
+
+	return nil
 }
 
 func (l *linter) readToken() error {
@@ -140,23 +110,6 @@ func (l *linter) readToken() error {
 	return nil
 }
 
-func (l *linter) parseFlags() error {
-	flag.StringVar(&l.user, "user", "",
-		`github user/organization name`)
-	flag.BoolVar(&l.verbose, "v", false,
-		`verbose mode that turns on additional debug output`)
-	flag.BoolVar(&l.skipForks, `skipForks`, true,
-		`whether to skip repositories that are forks`)
-
-	flag.Parse()
-
-	if l.user == "" {
-		return errors.New("-user argument can't be empty")
-	}
-
-	return nil
-}
-
 func (l *linter) initClient() error {
 	l.ctx = context.Background()
 
@@ -167,17 +120,7 @@ func (l *linter) initClient() error {
 	return nil
 }
 
-func newRepositoryListOptions() *github.RepositoryListOptions {
-	// Use some high value, github will limit it anyway,
-	// but we're interested in getting more data per one request.
-	return &github.RepositoryListOptions{
-		ListOptions: github.ListOptions{PerPage: math.MaxInt32},
-	}
-}
-
 func (l *linter) getReposList() error {
-	// TODO: collect only repos that were updated at least 6 months ago?
-
 	opts := newRepositoryListOptions()
 	for {
 		repos, resp, err := l.client.Repositories.List(l.ctx, l.user, opts)
@@ -186,15 +129,25 @@ func (l *linter) getReposList() error {
 		}
 
 		if l.verbose {
-			log.Printf("\tdebug: fetched %d repo names\n", len(repos))
+			log.Printf("\t\tdebug: fetched %d repo names\n", len(repos))
 		}
 		for _, repo := range repos {
 			if l.skipForks && *repo.Fork {
 				if l.verbose {
-					log.Printf("\tdebug: skip %s fork repo\n", *repo.Name)
+					log.Printf("\t\tdebug: skip %s fork repo", *repo.Name)
 				}
 				continue
 			}
+
+			const hoursToExpire = 365 * 24
+			inactive := time.Since(repo.GetPushedAt().Time).Hours() > hoursToExpire
+			if l.skipInactive && inactive {
+				if l.verbose {
+					log.Printf("\t\tdebug: skip %s inactive repo", *repo.Name)
+				}
+				continue
+			}
+
 			l.repos = append(l.repos, *repo.Name)
 		}
 
@@ -208,102 +161,103 @@ func (l *linter) getReposList() error {
 }
 
 func (l *linter) lintRepos() error {
+	l.repos = []string{"bad-repo"}
 	for i, repo := range l.repos {
-		log.Printf("\tchecking %s/%s (%d/%d)...",
+		log.Printf("\tchecking %s/%s (%d/%d) ...",
 			l.user, repo, i+1, len(l.repos))
 		l.lintRepo(repo)
 	}
 	return nil
 }
 
+type repoFile struct {
+	// origName is file original name as in github repo.
+	origName string
+
+	// baseName is a filepath.Base(origName) result.
+	baseName string
+
+	// tempName is a full filename on a local filesystem.
+	// If empty, no local file is associated.
+	tempName string
+
+	require struct {
+		localCopy bool
+	}
+}
+
 func (l *linter) lintRepo(repo string) {
-	// TODO: merge readme and other files linting?
-	l.lintReadme(repo)
-	l.lintFiles(repo)
-}
+	files := l.collectDirFiles(repo, "/")
 
-func (l *linter) lintFilenames(repo string, list []*github.RepositoryContent) {
-	// TODO: don't compile regular expressions for every call of lintFilenames.
-	patterns := []struct {
-		name string
-		re   *regexp.Regexp
-	}{
-		{"Vim swap", regexp.MustCompile(`^.*\.swp$`)},
-		{"Emacs autosave", regexp.MustCompile(`^#.*#$`)},
-		{"Emacs backup", regexp.MustCompile(`^.*~$`)},
-		{"Mac OS sys file", regexp.MustCompile(`^\.DS_STORE$`)},
-		{"Windows sys file", regexp.MustCompile(`^Thumbs\.db$`)},
-	}
-
-	for _, f := range list {
-		for _, pat := range patterns {
-			if pat.re.MatchString(*f.Name) {
-				log.Printf("%s: remove %s file: %s", repo, pat.name, *f.Name)
-				break // Can't match more than 1 kind
-			}
+	for name, c := range l.checkers {
+		c.Reset()
+		// Let it accept files it is interested in.
+		for _, f := range files {
+			c.PushFile(f)
+			l.resolveRequirements(repo, f)
+		}
+		// Now run check over all accepted files.
+		for _, warning := range c.CheckFiles() {
+			log.Printf("%s: %s: %s", repo, name, warning)
 		}
 	}
 }
 
-func (l *linter) lintFiles(repo string) {
-	// TODO: recurse into sub-directories.
-	// TODO: do multi-request if there is more files than github returns per 1 req.
-	_, list, _, err := l.client.Repositories.GetContents(l.ctx, l.user, repo, "/", nil)
+func (l *linter) collectDirFiles(repo, dir string) []*repoFile {
+	tree, _, err := l.client.Git.GetTree(l.ctx, l.user, repo, "master", true)
 	if err != nil {
-		panic(fmt.Sprintf("%s: list directory: %v", repo, err))
+		panic(fmt.Sprintf("get tree: %v", err))
 	}
-	l.lintFilenames(repo, list)
-	var checkers = map[string]fileChecker{
-		"CONTRIBUTING.md": &documentationChecker{},
-		"CONTRIBUTING":    &documentationChecker{},
+	if l.verbose && *tree.Truncated {
+		log.Printf("\t\tdebug: %s tree is truncated", repo)
 	}
-	for _, f := range list {
-		c := checkers[*f.Name]
-		if c != nil {
-			// To get file contents, another request must be performed.
-			f, _, _, err := l.client.Repositories.GetContents(l.ctx, l.user, repo, "/"+*f.Name, nil)
-			if err != nil {
-				panic(fmt.Sprintf("can't fetch listed file: %v", err))
-			}
-			for _, err := range c.CheckFile(l.tempFile(f)) {
-				log.Printf("%s: %s: %v", repo, *f.Name, err)
-			}
+
+	var files []*repoFile
+	for _, entry := range tree.Entries {
+		if entry.Path == nil {
+			continue
 		}
+		files = append(files, &repoFile{
+			origName: *entry.Path,
+			baseName: filepath.Base(*entry.Path),
+		})
+	}
+
+	return files
+}
+
+func (l *linter) resolveRequirements(repo string, f *repoFile) {
+	if f.require.localCopy && f.tempName == "" {
+		l.createLocalCopy(repo, f)
 	}
 }
 
-func (l *linter) tempFile(f *github.RepositoryContent) string {
-	filename := filepath.Join(l.tempDir, *f.Name)
-	data, err := f.GetContent()
-	if err != nil {
-		panic(fmt.Sprintf("get %s contents: %v", *f.Name, err))
-	}
+func (l *linter) createLocalCopy(repo string, f *repoFile) {
+	flatPath := strings.Replace(f.origName, "/", "_(slash)_", -1)
+	filename := filepath.Join(l.tempDir, flatPath)
+	data := l.getContents(repo, f.origName)
 	if err := ioutil.WriteFile(filename, []byte(data), 0644); err != nil {
-		panic(fmt.Sprintf("write %s: %v", *f.Name, err))
+		panic(fmt.Sprintf("write %s: %v", f.origName, err))
 	}
-	return filename
+	f.tempName = filename
 }
 
-func (l *linter) lintReadme(repo string) {
-	checks := []struct {
-		name    string
-		checker fileChecker
-	}{
-		{"broken link", &brokenLinksChecker{}},
-		{"misspell", &misspellChecker{}},
-	}
-
-	f, _, err := l.client.Repositories.GetReadme(l.ctx, l.user, repo, nil)
+func (l *linter) getContents(repo, path string) string {
+	f, _, _, err := l.client.Repositories.GetContents(l.ctx, l.user, repo, path, nil)
 	if err != nil {
-		log.Printf("%s: can't access README", repo)
-		return
+		panic(fmt.Sprintf("get %s contents: %v", path, err))
 	}
-	tmp := l.tempFile(f)
-
-	for _, c := range checks {
-		for _, err := range c.checker.CheckFile(tmp) {
-			log.Printf("%s: %s: %v", repo, c.name, err)
-		}
+	s, err := f.GetContent()
+	if err != nil {
+		panic(fmt.Sprintf("get %s contents: %v", path, err))
 	}
+	return s
+}
 
+func newRepositoryListOptions() *github.RepositoryListOptions {
+	// Use some high value, github will limit it anyway,
+	// but we're interested in getting more data per one request.
+	return &github.RepositoryListOptions{
+		ListOptions: github.ListOptions{PerPage: math.MaxInt32},
+	}
 }
